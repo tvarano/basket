f5f51ec82667dedbf869adba61dc806c41bdcb1c
Thomas Varano <thvarano@gmail.com>
2020-11-28 08:47:28 -0500

    small restructure updates

--- poolbet/contracts/Bet.sol
+++ poolbet/contracts/Bet.sol
@@ -1,5 +1,15 @@
 pragma solidity ^0.7.0;
+import "./BasketPlayer.sol";
+import "./Basket.sol";
 
 contract Bet {
+    // basket, 
+    Basket placedIn;
+    // basketplayer,
+    BasketPlayer placedBy; 
+    // matchname, 
+    string matchname;
+    // oddsplaced / for who, 
 
+    // when decided
 }
